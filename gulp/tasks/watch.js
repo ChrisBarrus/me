de119ccd899dce496319c3c190fe7bc06488c629
Krzysztof <krzysztofbaranski1986@gmail.com>
2018-01-17 12:58:09 +0100

    Completed large hero block and intro content block

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('watch', function(){
+
+    browserSync.init({
+
+        notify: false,
+        server: {
+
+            baseDir: "app"
+
+        }
+
+    });
+
+    watch('./app/index.html', function(){
+
+        browserSync.reload();
+
+    });
+
+    watch('./app/assets/styles/**/*.css', function(){
+
+        gulp.start('cssInject');
+
+    });
+
+});
+
+gulp.task('cssInject', ['styles'], function(){
+
+    //async function so return is needed
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+
+});
